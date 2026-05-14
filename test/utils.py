63538ca24a4fb07b4e0b85c252d392d8ba5207bc
amtrax <mikics2k17@gmail.com>
2026-05-14 17:39:07 +0200

    chore: code cleanup

--- test/utils.py
+++ test/utils.py
@@ -1,17 +1,19 @@
-# Exemplary calculator functions
-
+""" Exemplary calculator functions"""
 
 def add(a: int, b: int) -> int:
+    """Function for adding values"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Function for substracting"""
     return a - b
 
-
 def multiply(a: int, b: int) -> int:
+    """Function for multiplying"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Function for dividing."""
     return a / b
